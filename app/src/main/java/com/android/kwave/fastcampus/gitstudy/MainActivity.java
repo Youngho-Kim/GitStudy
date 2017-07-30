f534fb523fc4413b4c36d95247c5bf8ad51344f6
kwave <kwvae55@gmail.com>
2017-07-30 16:00:21 +0900

    실험 브랜치에 edittext 추가

--- app/src/main/java/com/android/kwave/fastcampus/gitstudy/MainActivity.java
+++ app/src/main/java/com/android/kwave/fastcampus/gitstudy/MainActivity.java
@@ -3,6 +3,7 @@ package com.android.kwave.fastcampus.gitstudy;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     private Button button2;
     private Button button3;
     private Button button4;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +25,6 @@ public class MainActivity extends AppCompatActivity {
         button2 = (Button) findViewById(R.id.button2);
         button3 = (Button) findViewById(R.id.button3);
         button4 = (Button) findViewById(R.id.button4);
+        editText = (EditText) findViewById(R.id.editText);
     }
 }
