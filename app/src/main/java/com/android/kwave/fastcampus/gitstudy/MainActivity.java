28481573ec2985c1ff67da3e315970a8d9c83799
kwave <kwvae55@gmail.com>
2017-07-30 15:59:31 +0900

    마스터에 텍스트뷰 생성

--- app/src/main/java/com/android/kwave/fastcampus/gitstudy/MainActivity.java
+++ app/src/main/java/com/android/kwave/fastcampus/gitstudy/MainActivity.java
@@ -3,12 +3,14 @@ package com.android.kwave.fastcampus.gitstudy;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button button;
     private Button button2;
     private Button button3;
+    private TextView textView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +23,6 @@ public class MainActivity extends AppCompatActivity {
         button = (Button) findViewById(R.id.button);
         button2 = (Button) findViewById(R.id.button2);
         button3 = (Button) findViewById(R.id.button3);
+        textView2 = (TextView) findViewById(R.id.textView2);
     }
 }
